932395d40d501f52fef709e41fa211f7412831e6
Dixit Kumar Hasija <dixit.hasija@shiprocket.com>
2024-11-14 13:24:42 +0530

    -

--- main.js
+++ main.js
@@ -4,9 +4,24 @@ import { setupFirebase } from './services/firebase.service';
 // import { setupFirebase } from "./old_firebase.service";
 // Call the function to initialize Firebase
 // import { initializeApp } from 'firebase/app';
+// index.js
+const loadServiceWorkerAndSetupFirebase = () => {
+    debugger
+    if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('https://dixithasija.github.io/engageLink/service-worker/firebase-messaging-sw.js')
+      .then((registration) => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch((error) => {
+        console.error('Service Worker registration failed:', error);
+      });
+  });
+}
+}
 
 window.wigzo_en = {
-    setupFirebase: setupFirebase
+    setupFirebase: loadServiceWorkerAndSetupFirebase
 }
 
 // export {initializeApp as firebase}
